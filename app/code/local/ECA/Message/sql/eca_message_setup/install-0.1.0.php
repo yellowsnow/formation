96dba976ae339c67df4a7529e8daea134dff5251
Gabriel Bouhatous - ECA <gabriel.bouhatous@ecommerce-academy.fr>
2016-09-28 15:38:10 +0100

    ECA_Message : script d'install

---
+++ app/code/local/ECA/Message/sql/eca_message_setup/install-0.1.0.php
@@ -0,0 +1,26 @@
+<?php
+/** @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+/** @var $adapter Varien_Db_Adapter_Pdo_Mysql */
+$adapter = $installer->getConnection();
+
+/**
+ * Create table 'eca_message/message'
+ */
+$table = $adapter->newTable($installer->getTable('eca_message/message'))
+    ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity' => true,
+        'unsigned' => true,
+        'nullable' => false,
+        'primary'  => true,
+    ), 'Message ID')
+    ->addColumn('title', Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        255, array('nullable'  => false), 'Title')
+    ->addColumn('content', Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        255, array('nullable'  => false), 'Content');
+$adapter->createTable($table);
+
+$installer->endSetup();
