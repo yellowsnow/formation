3a06f4911d59f0982f30771295e8ac3bd6f587b5
Gabriel Bouhatous - ECA <gabriel.bouhatous@ecommerce-academy.fr>
2016-09-28 11:52:14 +0100

    Module ECA_Message : * fichier de déclaration dans app/etc * fichier de configuration * block + template form * layout dédié * fichier de traduction

---
+++ app/code/local/ECA/Message/Block/Form.php
@@ -0,0 +1,13 @@
+<?php
+class ECA_Message_Block_Form extends Mage_Core_Block_Template
+{
+    /**
+     * Hello World test function
+     *
+     * @return string
+     */
+    public function getHelloWorld()
+    {
+        return $this->__('Hello World !');
+    }
+}
