5071113f628b44590c50d9d7cfaf5303db187549
Gabriel Bouhatous - ECA <gabriel.bouhatous@ecommerce-academy.fr>
2016-09-29 11:59:44 +0100

    ECA_Message : grid admin

---
+++ app/code/local/ECA/Message/Block/Adminhtml/Message/Grid.php
@@ -0,0 +1,45 @@
+<?php
+class ECA_Message_Block_Adminhtml_Message_Grid  extends Mage_Adminhtml_Block_Widget_Grid
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('adminhtmlMessageGrid');
+        $this->setDefaultSort('id');
+        $this->setDefaultDir('DESC');
+        $this->setSaveParametersInSession(true);
+    }
+
+    protected function _prepareCollection()
+    {
+        $messageCollection = Mage::getModel('eca_message/message')->getCollection();
+        $this->setCollection($messageCollection);
+
+        parent::_prepareCollection();
+        return $this;
+    }
+
+    protected function _prepareColumns()
+    {
+        $this->addColumn('id', array(
+            'header' => $this->helper('eca_message')->__('Message Id'),
+            'index'  => 'id',
+            'align'  => 'left',
+        ));
+
+        $this->addColumn('title', array(
+            'header' => $this->helper('eca_message')->__('Message Title'),
+            'index'  => 'title',
+            'align'  => 'left',
+        ));
+
+        $this->addColumn('content', array(
+            'header' => $this->helper('eca_message')->__('Message Content'),
+            'index'  => 'content',
+            'align'  => 'right',
+        ));
+
+        parent::_prepareColumns();
+        return $this;
+    }
+}
